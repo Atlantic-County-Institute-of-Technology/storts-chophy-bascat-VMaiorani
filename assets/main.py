0f2a8968cd1bdc6b5a6ab18b65f8def66a1d56d7
Vince <maio9545@student.acitech.org>
2025-11-18 10:47:26 -0500

    Finish

--- assets/main.py
+++ assets/main.py
@@ -1,8 +1,7 @@
 import random
 
 WORD_LIST = []
-LETTERS_LEN = 5
-
+Lives = 4
 
 def generateword():
     try:
@@ -12,7 +11,6 @@ def generateword():
                     WORD_LIST.append(word.strip())
     except FileNotFoundError:
         print("[!] Error! File not Found.")
-    return word
 
 def replay():
     while True:
@@ -27,35 +25,58 @@ def replay():
             print("pls select an option available")
     main()
 
+def difficulty():
+    global LETTERS_LEN
+    while True:
+        dif = int(input("Please select a difficulty [ 1 = EASY, 2 = MEDIUM, 3 = HARD ] :"))
+        if dif == 1:
+            print("You selected difficulty EASY, the word is 4 letters long")
+            LETTERS_LEN = 4
+            break
+        elif dif == 2:
+            print("You selected difficulty MEDIUM, the word is 5 letters long")
+            LETTERS_LEN = 5
+            break
+        elif dif == 3:
+            print("You selected difficulty HARD, the word is 6 letters long")
+            LETTERS_LEN = 6
+            break
+        else:
+            print("pls select an option available")
+
 def main():
     lives = 4
+    difficulty()
     generateword()
     target = WORD_LIST[random.randint(0, len(WORD_LIST))]
     print(target)
 
-    # if Lives >= 0:
     while lives >= 0:
-        temp = 0
+        print(LETTERS_LEN)
         guess = input("please type in your guess: ")
-        response = ["Bacat" for i in range(len(target))]
-        if len(guess) == 5:
-            if guess == target:
-                print("Correct: You Win")
-                break
-            else:
-                print("current tries: " + str(lives))
-                for g_digit in range(len(guess)):
-                    for t_digit in range(len(target)):
-                        if guess[g_digit] == target[t_digit]:
-                            if g_digit == t_digit:
-                                response[g_digit] = str(guess[temp]) + " = Chophy"
-                            else:
-                                response[g_digit] = str(guess[temp]) + " = Storts"
-                            temp += 1
-                print(response)
-            lives -= 1
+        response = []
+        for letter in guess:
+            response.append(f"{letter} = Bascat")
+        if len(guess) == len(target):
+            if guess in WORD_LIST:
+                if guess == target:
+                    print("Correct: You Win")
+                    break
+                else:
+                    print("current tries: " + str(lives))
+                    for g_digit in range(len(guess)):
+                        for t_digit in range(len(target)):
+                            if guess[g_digit] == target[t_digit]:
+                                if g_digit == t_digit:
+                                    response[g_digit] = str(guess[g_digit]) + " = Chophy"
+                                else:
+                                    response[g_digit] = str(guess[g_digit]) + " = Storts"
+                    print(response)
+                lives -= 1
+            elif guess not in WORD_LIST:
+                print("Word not found in the dictionary")
         else:
-            print("please enter a 5 letter word")
+            print("please enter a letter word that is at least " + str(len(target)))
     replay()
 
 if __name__ == '__main__':
